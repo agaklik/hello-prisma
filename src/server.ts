b874e422e03a941b776961baa3812639f8e65e21
agaklik <112034103+agaklik@users.noreply.github.com>
2023-06-30 22:30:42 -0300

    Commit inicial

---
+++ src/server.ts
@@ -0,0 +1,48 @@
+import exp from 'constants'
+import express from 'express' 
+import { PrismaClient } from '@prisma/client'
+
+
+const prisma = new PrismaClient()
+const server = express()
+const port = 3333
+
+server.use(express.json())
+
+server.get('/', async (req,res) => {
+    // res.send('Hello World 😁')
+    const getAll = await prisma.user.findMany()
+    res.json(getAll)
+})
+
+interface IRequest {
+    name: string
+    email: string
+
+}
+
+server.post('/', async (req, res) => {
+    const { name , email }: IRequest = req.body
+
+    const userExist = await prisma.user.findFirst({
+        where:{
+            email
+        }
+    })
+
+    if(userExist) return res.status(404).json({error:true, message: "Usuário já existe"})
+
+    const createUser = await prisma.user.create({
+        data: {
+            name,
+            email
+        }
+    })
+
+    res.json(createUser)
+
+})
+
+server.listen(port, () => {
+    console.log(`Server in running - http://localhost:${port}`);
+})
